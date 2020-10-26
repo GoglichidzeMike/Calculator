aff7b9e5efbb76db54246bdf99d5d6cc64bc5384
GoglichidzeMike <goglichidze.mike@gmail.com>
2020-10-26 20:56:31 +0400

    Updates

---
+++ precache-manifest.0afe7c516dba3200628a86f024ca35e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9c20c1757103f7f7f742a55c2c96edb",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "e00f52ccdeb1ab8a0236",
+    "url": "/Calculator/static/css/main.15f50850.chunk.css"
+  },
+  {
+    "revision": "3bcdd3f1fa2b8e397372",
+    "url": "/Calculator/static/js/2.5cd44e81.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/Calculator/static/js/2.5cd44e81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e00f52ccdeb1ab8a0236",
+    "url": "/Calculator/static/js/main.d5e2229f.chunk.js"
+  },
+  {
+    "revision": "56f8de223182093d6a56",
+    "url": "/Calculator/static/js/runtime-main.aa9c4a47.js"
+  }
+]);
